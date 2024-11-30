aca70f2067397c85aed20c7198af267240b9ca13
yaseminvatan <66827861+yaseminvatan@users.noreply.github.com>
2024-11-29 20:59:07 -0800

    getRandomEmployee function will select a random employee from the array and log their full name to the console.

--- Develop/script.js
+++ Develop/script.js
@@ -44,6 +44,14 @@ const displayAvarageSalary = function (employeesArray) {
 // Select a random employee
 const getRandomEmployee = function(employeesArray) {
   // TODO: Select and display a random employee
+  const getRandomEmployee = function (employeesArray) {
+    if (employeesArray.length === 0) 
+      return;
+    const randomIndex = Math.floor(Math.random() * employeesArray.length);
+    const randomEmployee = employeesArray[randomIndex]
+    console.log(`Random Employee: ${randomEmployee.firstName} ${randomEmployee.lastName}`);
+  };
+  
 }
 
 /*
